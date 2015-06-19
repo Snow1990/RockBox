3260d580c635a2f22ce66acb93ac5e65e4f5ea23
Snow1990 <snn@shaonandeMacBook-Pro.local>
2015-06-19 09:22:00 +0800

    箱子自由落体碰撞效果

--- RockBox/ViewController.swift
+++ RockBox/ViewController.swift
@@ -10,8 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Most conservative guess. We'll set them later.
+    var maxX : CGFloat = 320
+    var maxY : CGFloat = 320
+    let boxSize : CGFloat = 30.0
+    var boxes : Array<UIView> = []
+    var animator:UIDynamicAnimator?
+    let gravity = UIGravityBehavior()
+    let collider = UICollisionBehavior()
+
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        maxX = super.view.bounds.size.width - boxSize
+        maxY = super.view.bounds.size.height - boxSize
+//        addBox(CGRectMake(100, 100, 30, 30))
+        createAnimatorStuff()
+        generateBoxes()
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +37,79 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func randomColor() -> UIColor {
+        let red = CGFloat(arc4random()%100000)/100000
+        let green = CGFloat(arc4random()%100000)/100000
+        let blue = CGFloat(arc4random()%100000)/100000
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: 0.85)
+    }
 
+    func doesNotCollide(testRect: CGRect) -> Bool {
+        for box in boxes {
+            var viewRect = box.frame
+            if CGRectIntersectsRect(testRect, viewRect) {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func randomFrame() -> CGRect {
+        var guess = CGRectMake(9, 9, 9, 9)
+        
+        do {
+        
+        let guessX = CGFloat(arc4random()) % maxX
+        let guessY = CGFloat(arc4random()) % maxY
+        guess = CGRectMake(guessX, guessY, boxSize, boxSize)
+        
+        }while(!doesNotCollide(guess))
+        return guess
+    }
+    
+    
+    func addBox(location: CGRect, color: UIColor) -> UIView {
+        let newBox = UIView(frame: location)
+        newBox.backgroundColor = color
+
+        view.addSubview(newBox)
+        addBoxBehaviors(newBox)
+        
+        boxes.append(newBox)
+
+        return newBox
+    }
+    
+    func generateBoxes() {
+        for i in 0...15 {
+            var frame = randomFrame()
+            var color = randomColor()
+            var newBox = addBox(frame, color: color)
+        }
+    }
+    
+    func createAnimatorStuff() {
+        animator = UIDynamicAnimator(referenceView:self.view)
+        
+        collider.translatesReferenceBoundsIntoBoundary = true
+        animator?.addBehavior(collider)
+        
+
+        gravity.gravityDirection = CGVectorMake(0, 0.8)
+        animator?.addBehavior(gravity)
+    }
+
+    
+    func addBoxBehaviors(box: UIView) {
+        gravity.addItem(box)
+        collider.addItem(box)
+    }
+    
+    
+    
+    
 
 }
 
